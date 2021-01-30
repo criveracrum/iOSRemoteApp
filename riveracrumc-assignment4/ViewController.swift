ca26edf43f5b16ea4427d701e806b3c1ae67f55e
Chad <criveracrum@gmail.com>
2021-01-30 10:13:02 -0600

    Initial Commit

---
+++ riveracrumc-assignment4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  riveracrumc-assignment4
+//
+//  Created by Chadwick on 1/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
